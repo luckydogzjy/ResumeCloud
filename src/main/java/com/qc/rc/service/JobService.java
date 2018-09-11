ac3a185654d629543be8c4763dae350e59d63a6b
qiancheng <qiancheng@192.168.199.140>
2018-09-11 18:14:51 +0800

    0

--- src/main/java/com/qc/rc/service/JobService.java
+++ src/main/java/com/qc/rc/service/JobService.java
@@ -1,53 +1,6 @@
 package com.qc.rc.service;
 
-import java.util.List;
-
-import com.qc.rc.entity.Job;
 
 public interface JobService {
-	/**
-	 * @author qiancheng
-	 * 检索所有job
-	 * @param userId
-	 * @return
-	 */
-	public List<Job> jobGetAll(Integer userId);
-	/**
-	 * 添加一个job、
-	 * @param job
-	 * @return
-	 */
-	public boolean jobAdd(Job job);
-	/**
-	 * 更新一条job
-	 * @param job
-	 * @return
-	 */
-	public boolean jobUpdate(Job job);
-	/**
-	 * 根据jobId删除一条job
-	 * @param jobId
-	 * @return
-	 */
-	public boolean jobDelete(Integer jobId);
-	/**
-	 * 变更状态
-	 * @param jobId
-	 * @param jobStatus
-	 * @return
-	 */
-	public boolean jobChangeStatus(Integer jobId,Integer jobStatus);
-	/**
-	 * 模糊查询
-	 * @param userId
-	 * @param jobName
-	 * @return
-	 */
-	public List<Job> jobGetByName(Integer userId,String jobName);
-	/**
-	 * 查看职位详情
-	 * @param jobId
-	 * @return
-	 */
-	public Job jobGetOne(Integer jobId);
+	
 }
