cc622192269ea5efd253d7f0eb5dcad4ae422fad
q632786948 <ljy6327@vip.qq.com>
2018-09-29 17:18:50 +0800

    11111

--- src/main/java/com/qc/rc/service/InterviewService.java
+++ src/main/java/com/qc/rc/service/InterviewService.java
@@ -1,15 +1,8 @@
 package com.qc.rc.service;
 
-import java.text.ParseException;
-import java.util.Date;
+
 import java.util.List;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import com.github.pagehelper.PageInfo;
 import com.qc.rc.common.ServerResponse;
-import com.qc.rc.entity.Resume;
-import com.qc.rc.entity.User;
 import com.qc.rc.entity.pojo.InterviewPojo;
 import com.qc.rc.entity.pojoView.InterviewPojoView;
 
@@ -17,10 +10,10 @@ public interface InterviewService {
 
 	
 	//Liu
-	//根据ResumeId查询详细信息
+	//根据InterviewId查询详细信息
 	public ServerResponse<InterviewPojo> getInterviewByInterviewId(String InterviewId);
 		
-	//根据ResumeId更新面试信息
+	//根据InterviewId更新面试信息
 	public ServerResponse updateInterviewsByInterviewId(InterviewPojo interviewPojo,String ResumeId,String resumePhone);
 	
 	//根据userId查询所有的面试安排
