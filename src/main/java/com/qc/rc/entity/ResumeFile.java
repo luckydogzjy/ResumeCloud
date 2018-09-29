e2479ce1ef6263ae18ac26d1c2d3fc0690afbced
luckydogzjy <476518267@qq.com>
2018-09-29 16:58:07 +0800

    111

---
+++ src/main/java/com/qc/rc/entity/ResumeFile.java
@@ -0,0 +1,93 @@
+package com.qc.rc.entity;
+
+import java.util.Date;
+
+
+public class ResumeFile {
+	
+	private String fId;
+	private String fResumeId;
+	private String fFile;  //文件路径
+	
+	private String fCreateUser;
+	private Date fCreateTime;
+	private String fUpdateUser;
+	private Date fUpdateTime;
+	private Date fDeleteFlag;
+	public String getfId() {
+		return fId;
+	}
+	public void setfId(String fId) {
+		this.fId = fId;
+	}
+	public String getfResumeId() {
+		return fResumeId;
+	}
+	public void setfResumeId(String fResumeId) {
+		this.fResumeId = fResumeId;
+	}
+	public String getfFile() {
+		return fFile;
+	}
+	public void setfFile(String fFile) {
+		this.fFile = fFile;
+	}
+	public String getfCreateUser() {
+		return fCreateUser;
+	}
+	public void setfCreateUser(String fCreateUser) {
+		this.fCreateUser = fCreateUser;
+	}
+	public Date getfCreateTime() {
+		return fCreateTime;
+	}
+	public void setfCreateTime(Date fCreateTime) {
+		this.fCreateTime = fCreateTime;
+	}
+	public String getfUpdateUser() {
+		return fUpdateUser;
+	}
+	public void setfUpdateUser(String fUpdateUser) {
+		this.fUpdateUser = fUpdateUser;
+	}
+	public Date getfUpdateTime() {
+		return fUpdateTime;
+	}
+	public void setfUpdateTime(Date fUpdateTime) {
+		this.fUpdateTime = fUpdateTime;
+	}
+	public Date getfDeleteFlag() {
+		return fDeleteFlag;
+	}
+	public void setfDeleteFlag(Date fDeleteFlag) {
+		this.fDeleteFlag = fDeleteFlag;
+	}
+	/**
+	 * @param fId
+	 * @param fResumeId
+	 * @param fFile
+	 * @param fCreateUser
+	 * @param fCreateTime
+	 * @param fUpdateUser
+	 * @param fUpdateTime
+	 * @param fDeleteFlag
+	 */
+	public ResumeFile(String fId, String fResumeId, String fFile, String fCreateUser, Date fCreateTime, String fUpdateUser,
+			Date fUpdateTime, Date fDeleteFlag) {
+		super();
+		this.fId = fId;
+		this.fResumeId = fResumeId;
+		this.fFile = fFile;
+		this.fCreateUser = fCreateUser;
+		this.fCreateTime = fCreateTime;
+		this.fUpdateUser = fUpdateUser;
+		this.fUpdateTime = fUpdateTime;
+		this.fDeleteFlag = fDeleteFlag;
+	}
+	
+	public ResumeFile(){
+		super();
+	}
+	
+	
+}
